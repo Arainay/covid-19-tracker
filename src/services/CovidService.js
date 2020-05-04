49a2ff324a3fac7c6f76afc03314cc38f141af5e
arainay <arainay@gmail.com>
2020-05-04 08:08:42 +0800

    Added main API methods

--- src/services/CovidService.js
+++ src/services/CovidService.js
@@ -4,4 +4,24 @@ export default class CovidService extends ApiService {
   getInfo() {
     return this.get();
   }
+
+  getCountries() {
+    return this.get('/countries');
+  }
+
+  getCountryByName(name) {
+    return this.get(`/counties/${name}`);
+  }
+
+  getConfirmed() {
+    return this.get('/confirmed');
+  }
+
+  getDaily() {
+    return this.get('/daily');
+  }
+
+  getRecovered() {
+    return this.get('/recovered');
+  }
 }
